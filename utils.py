93ccd35d1e4b8691b457d38346439627d73eac68
krassowski <krassowski.michal@gmail.com>
2020-06-14 16:09:16 +0100

    Remove batches function (belongs to easy_entrez)

--- utils.py
+++ utils.py
@@ -1,14 +1,6 @@
 import xmltodict
 import json
 from xml.etree import ElementTree
-from math import ceil
-
-
-def batches(data, batch_size=100):
-    return [
-        data[i * batch_size:(i + 1) * batch_size]
-        for i in range(0, ceil(len(data) / batch_size))
-    ]
 
 
 def xml_element_to_json(element: ElementTree.Element):
