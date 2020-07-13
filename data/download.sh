68aeec8b1a16c21d18eb5ee6dc9a1ddc4a358dfb
krassowski <krassowski.michal@gmail.com>
2020-07-13 11:07:01 +0100

    Add some initial feature engineering (species + diseases)

--- data/download.sh
+++ data/download.sh
@@ -1,1 +1,10 @@
 wget https://ftp.ncbi.nlm.nih.gov/pub/pmc/PMC-ids.csv.gz
+wget https://ftp.ncbi.nlm.nih.gov/pub/clinvar/disease_names
+mkdir -p taxdump
+cd taxdump
+wget https://ftp.ncbi.nih.gov/pub/taxonomy/taxdmp.zip
+unzip taxdmp.zip
+cd ..
+
+# 812 MB for spacy 2.0, the most accurate model (https://spacy.io/usage/facts-figures#benchmarks-models-english)
+python -m spacy download en_core_web_lg
