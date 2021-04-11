83ab02ad0f68c85e2f4a5f1ebc86ac8aae3fc6f1
krassowski <krassowski.michal@gmail.com>
2021-04-11 15:50:14 +0100

    Remove reverse_log_trans (a better version is now provided by ComplexUpset)

--- helpers/plots.R
+++ helpers/plots.R
@@ -5,16 +5,3 @@ requireNamespace('ggrepel', quietly=TRUE)
 requireNamespace('shadowtext', quietly=TRUE)
 requireNamespace('ComplexUpset', quietly=TRUE)
 
-
-reverse_log_trans <- function(base=10) {
-    # CC-BY-SA 4.0 Brian Diggs, modified
-    # https://stackoverflow.com/a/11054781
-    scales::trans_new(
-        paste0(
-            'reverselog-', format(base)),
-            function(x) -log(x, base),
-            function(x) base^-x,
-            scales::log_breaks(base=base),
-            domain = c(1e-100, Inf)
-    )
-}
